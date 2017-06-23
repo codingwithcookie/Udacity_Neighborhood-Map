d2218d8594806cba451f9983b7019824e9aa7a39
Andrew Cook <codingwithcookie@outlook.com>
2017-06-22 21:25:07 -0700

    Updated to async

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,11 @@ var map;
 
 var trailheads = [];
 
+var viewModel = {
+    trailheads: ko.observableArray(),
+    filter: ko.observable('')
+};
+
 function generateMap() {
     map = new google.maps.Map(document.getElementById('map'), {
     zoom: 11,
@@ -9,8 +14,10 @@ function generateMap() {
     disableDefaultUI: true});   
 
     locations.forEach(function(location) {
-    var data = GetWeatherData(location.Lat, location.Long);
-    var marker = new google.maps.Marker({
+    GetWeatherData(location.Lat, location.Long).done(function(data) {
+        var temp =data.main.temp;
+        var description = data.weather[0].description;
+        var marker = new google.maps.Marker({
             position: {lat: location.Lat, lng: location.Long},
             map: map,
             animation: google.maps.Animation.DROP,
@@ -18,7 +25,7 @@ function generateMap() {
         });
 
         var infoWindow = new google.maps.InfoWindow({
-                content: location.name + ' ' + data.temp + ' °C'
+                content: location.name + ' ' + temp + ' °C'
             });
             marker.addListener('click', function() {
                 toggleBounce(marker);
@@ -29,51 +36,46 @@ function generateMap() {
             name: location.name, 
             lat: location.Lat, 
             long: location.Long,
-            temp: data.temp + ' °C',
-            description: data.description,
+            temp: temp + ' °C',
+            description: description,
             marker: marker
         };
 
-
-        trailheads.push(trailhead);
+        viewModel.trailheads.push(trailhead);
+    });
     }, this);
 
-    var viewModel = {
-    trailheads: ko.observableArray(trailheads),
-    filter: ko.observable('')
-};
+    var stringStartsWith = function (string, startsWith) {          
+        string = string || "";
+        if (startsWith.length > string.length)
+            return false;
+        return string.substring(0, startsWith.length) === startsWith;
+    };
 
-var stringStartsWith = function (string, startsWith) {          
-    string = string || "";
-    if (startsWith.length > string.length)
-        return false;
-    return string.substring(0, startsWith.length) === startsWith;
-};
+    viewModel.filteredItems = ko.dependentObservable(function() {
+        var filter = this.filter().toLowerCase();
+        if (!filter) {
+            this.trailheads().forEach(function(element) {
+                element.marker.setVisible(true);
+            }, this);
+            return this.trailheads();
+        } else {
+            return ko.utils.arrayFilter(this.trailheads(), function(trailhead) {
+                var include = stringStartsWith(trailhead.name.toLowerCase(), filter);
+                if (include)
+                {
+                    trailhead.marker.setVisible(true);
+                }
+                else
+                {
+                    trailhead.marker.setVisible(false);
+                }
+                return include;
+            });
+        }
+    }, viewModel);
 
-viewModel.filteredItems = ko.dependentObservable(function() {
-    var filter = this.filter().toLowerCase();
-    if (!filter) {
-        this.trailheads().forEach(function(element) {
-            element.marker.setVisible(true);
-        }, this);
-        return this.trailheads();
-    } else {
-        return ko.utils.arrayFilter(this.trailheads(), function(trailhead) {
-            var include = stringStartsWith(trailhead.name.toLowerCase(), filter);
-            if (include)
-            {
-                trailhead.marker.setVisible(true);
-            }
-            else
-            {
-                trailhead.marker.setVisible(false);
-            }
-            return include;
-        });
-    }
-}, viewModel);
-
-ko.applyBindings(viewModel);
+    ko.applyBindings(viewModel);
 }
 
 function toggleBounce(marker) {
@@ -86,43 +88,6 @@ function toggleBounce(marker) {
         setTimeout(function(){ marker.setAnimation(null); }, 1500);
     }
 };
-
-// var viewModel = {
-//     trailheads: ko.observableArray(trailheads),
-//     filter: ko.observable('')
-// };
-
-// var stringStartsWith = function (string, startsWith) {          
-//     string = string || "";
-//     if (startsWith.length > string.length)
-//         return false;
-//     return string.substring(0, startsWith.length) === startsWith;
-// };
-
-// viewModel.filteredItems = ko.dependentObservable(function() {
-//     var filter = this.filter().toLowerCase();
-//     if (!filter) {
-//         this.trailheads().forEach(function(element) {
-//             element.marker.visible = true;
-//         }, this);
-//         return this.trailheads();
-//     } else {
-//         return ko.utils.arrayFilter(this.trailheads(), function(trailhead) {
-//             var include = stringStartsWith(trailhead.name.toLowerCase(), filter);
-//             if (include)
-//             {
-//                 trailhead.marker.visible=true;
-//             }
-//             else
-//             {
-//                 trailhead.marker.visible =false;
-//             }
-//             return include;
-//         });
-//     }
-// }, viewModel);
-
-// ko.applyBindings(viewModel);
 
 function generateMapError() {
     console.log('Failure in loading Goggle Map.');
--- scripts/weather.js
+++ scripts/weather.js
@@ -3,13 +3,15 @@ function GetWeatherData(lat, long)
 {
     var result = {temp: 0,
     description: ''};
-    $.ajax({url: "http://api.openweathermap.org/data/2.5/weather?lat=" + encodeURIComponent(lat) + 
-                "&lon="+ encodeURIComponent(long) +"&units=metric&appid=5dff28ed35d569235c2b02c4fa76cdc9", 
-            success: function( data ) {
-                result.temp = data.main.temp;
-                result.description = data.weather[0].description;
-            },
-            async: false
-    });
-    return result;
+    return $.get( "http://api.openweathermap.org/data/2.5/weather?lat=" + encodeURIComponent(lat) + 
+                "&lon="+ encodeURIComponent(long) +"&units=metric&appid=5dff28ed35d569235c2b02c4fa76cdc9")
+                .done(function(data){
+                    result.temp = data.main.temp;
+                    result.description = data.weather[0].description;
+                    return result;
+                })
+   .fail(function(){
+        console.log('Error retriving weather data.');
+        alert('Unable to retrive weather data. Please try again.');
+    });    
 }
